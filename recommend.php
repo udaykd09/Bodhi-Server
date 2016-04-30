e1c2e0cb96629f442164dc9d337f449382239aad
Manasa Potnuru <manasa.potnuru7@gmail.com>
2016-04-29 19:45:18 -0700

    Update recommend.php

--- recommend.php
+++ recommend.php
@@ -1,18 +1,26 @@
 <?php
+  include 'moisture_estimation.php';
+  include 'plant_water_requirement.php';
+  include 'live_weather_prediction.php';
   $city = $_GET['city'];
-  $jsonurl = "http://api.openweathermap.org/data/2.5/weather?q=" . $city . "&appid=3188f906a781b8e7a1312bc5b2a0a59f";
-  $json = file_get_contents($jsonurl);
-  $cweather = json_decode($json);
-  $kelvin = $cweather->main->temp;
-  $fheat = ($kelvin - 273.15) * 1.8 + 32;
-  echo  "Temperature : " . $fheat . " fahrenheit."; 
-  echo "<br>";
-  $humidity = $cweather->main->humidity;
-  echo "Humidity : " . $humidity;
-  echo "<br>";
-  echo "Weather condition : ";
-  foreach($cweather->weather as $weath) {
-        $rain = $weath->main . ", " . $weath->description;
-        echo $rain;
+  $weather_cond = live_weather_pred($city);
+  $weather_sum = "Weather prediction is " . $weather_cond;
+  $plant_name = $_GET['plantname'];
+  $plant_water_req = required_waterlevel($plant_name);
+  $moisture = $_GET['moisture'];
+  $moisture_lvl = moisture_level($moisture);
+ 
+  // If condition is rain or moisture level is high. Dont water.
+  // If moisture level is low, plant is moist. Water.
+  // If moisture level is medium, plan is moist. Water.
+  // If moisture level is meidum, plant is dry. Dont water.
+  if ($weather_cond == "rain" || $moisture_lvl == "high") {
+    echo "Don't water. " . $weather_sum;
+  } else if (($moisture_lvl == "low" || $moisture_lvl == "medium") && $plant_water_req == "moist") {
+    echo "Please water. " . $weather_sum;
+  } else if ($moisture_lvl == "medium" && $plant_water_req == "dry") {
+    echo "Don't water. " . $weather_sum;
+  } else {
+    echo "Please water. " . $weather_sum;
   }
 ?>
